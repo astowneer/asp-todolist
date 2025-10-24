9f35375ccb02574a67bf4f1f014e47716d32856d
astowner <the1samevl@gmail.com>
2025-10-24 21:19:46 +0300

    feat: add login form td-3

--- frontend/src/components/auth/login/page.tsx
+++ frontend/src/components/auth/login/page.tsx
@@ -1,51 +1,28 @@
-import z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useLogin } from "./use-login";
-
-const loginSchema = z.object({
-  username: z
-    .string({
-      error: "Username is required",
-    })
-    .min(5, "Username should be at least 5 characters"),
-  password: z
-    .string({
-      error: "Password is required",
-    })
-    .min(6, "Password should be at least 6 characters"),
-});
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { LoginForm } from "./form";
 
 export default function Login() {
-  const {
-    reset,
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(loginSchema),
-  });
-
-  const { isPending, handleLogin } = useLogin(reset);
-
-  const onSubmit = handleSubmit(handleLogin);
-
   return (
-    <main>
-      <h1 className="font-light">Login</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" {...register("username")} />
-        {errors.username && <div>{errors.username.message}</div>}
-        <input type="text" {...register("password")} />
-        {errors.password && <div>{errors.password.message}</div>}
-        <button type="submit" disabled={isPending}>
-          Submit
-        </button>
-      </form>
-      <span>
-        Don't have an account?
-        <a href="/register">Register</a>
-      </span>
+    <main className="grow flex flex-col pt-[200px] items-center">
+      <Card className="w-full max-w-[400px]">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+        <CardFooter>
+          <p className="text-sm text-muted-foreground [&_a]:underline [&_a]:text-primary">
+            Don't have an account? <a href="/register">Register</a>
+          </p>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
